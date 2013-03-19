23d934d98c17388281761626c49e49aad508c1c0
zwy1135 <zwy1135@gmail.com>
2013-03-19 23:17:32 +0800

    第一次上传
    到第五章，模型，新版本Django app的建立不需加project名

---
+++ mysite2/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+from view import *
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    url('^time/$',current_datetime),
+    url(r'^time/plus/(\d{1,2})',hours_ahead),
+    # Examples:
+    # url(r'^$', 'mysite2.views.home', name='home'),
+    # url(r'^mysite2/', include('mysite2.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
